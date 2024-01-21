681a4559fee5e7d704c9a9b58e8ad8099323349f
AryaanHabib <habibaryaan@gmail.com>
2024-01-21 11:03:09 -0800

    yes

---
+++ frontend/main.js
@@ -0,0 +1,10 @@
+// main.js
+
+// Function to load the home.html file
+function loadHomePage() {
+    // Replace the path with the correct path to your home.html file
+    window.location.href = './views/home/home.html';
+}
+
+// Call the loadHomePage function when the script is loaded
+loadHomePage();
